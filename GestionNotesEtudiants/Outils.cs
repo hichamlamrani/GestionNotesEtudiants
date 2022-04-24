c20aad8781fb026b9b5109dfd53ad36b86f91c9e
Hicham Lamrani <2695453@collegelacite.ca>
2022-04-23 23:37:02 -0400

    Outils data save success

--- GestionNotesEtudiants/Outils.cs
+++ GestionNotesEtudiants/Outils.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,28 @@ namespace GestionNotesEtudiants
 {
     class Outils
     {
+        static string DataFolder = "DataFolder";
+
+        public static void saveDataToFile(Etudiant etudiant)
+        {
+            Directory.CreateDirectory(Outils.DataFolder);
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string path = Path.Combine(Path.Combine(currentDirectory, Outils.DataFolder), etudiant.getNumeroEtudiant().ToString()) + ".json";
+
+            string serializedEtudiant = JsonConvert.SerializeObject(etudiant);
+            File.WriteAllText(@path, serializedEtudiant);
+        }
+
+        public static void saveListDataToFiles(List<Etudiant> etudiants)
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            foreach (Etudiant etudiant in etudiants)
+            {
+                Outils.saveDataToFile(etudiant);
+            }
+        }
+
+        
 
     }
 }
